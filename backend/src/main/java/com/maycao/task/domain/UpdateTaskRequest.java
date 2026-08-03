8d531410d838a7e03346beb7944fad4301f78fd0
mayyuchencao <yuchencao260@gmail.com>
2026-08-03 02:42:28 -0400

    Fix errors

--- backend/src/main/java/com/maycao/task/domain/UpdateTaskRequest.java
+++ backend/src/main/java/com/maycao/task/domain/UpdateTaskRequest.java
@@ -1,5 +1,9 @@
 package com.maycao.task.domain;
 
+import com.maycao.task.domain.entity.TaskPriority;
+import com.maycao.task.domain.entity.TaskStatus;
+import java.time.LocalDate;
+
 public record UpdateTaskRequest (
     String title,
     String description,
