bb783da93d09bfaf47943d12d069ab646615850b
mayyuchencao <yuchencao260@gmail.com>
2026-08-03 01:50:22 -0400

    Add global exception handler

---
+++ backend/src/main/java/com/maycao/task/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.maycao.task.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.devtiro.task.domain.dto.ErrorDto;
+import com.devtiro.task.exception.TaskNotFoundException;
+import java.util.UUID;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+    String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+      .findFirst()
+      .map(DefaultMessageSourceResolvable::getDefaultMessage)
+      .orElse("Validation Failed.");
+
+    ErrorDto errorDto = new ErrorDto(errorMessage);
+    return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+  }
+}
